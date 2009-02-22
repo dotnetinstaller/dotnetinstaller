d89876d592b3963170cc58ff1d5995f0cf1770b9
dblock <none@none.com>
2009-02-22 17:41:53 +0000

    Changed version project from C++ makefile to C#. This removes an MSBuild warning and allows debugging InstallerEditor or dotNetInstaller without an annoying Visual Studio prompt of whether to rebuild the version project. Also got rid of redundant CPU configurations. Copied from https://dotnetinstaller.svn.sourceforge.net/svnroot/dotnetinstaller/trunk/, rev. 43 by dblockdotorg @ 7/9/2008 2:11 PM

---
+++ Version/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Version")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("1F40BF7E-4B2F-4D54-B615-1C4A4BFEB324")]
