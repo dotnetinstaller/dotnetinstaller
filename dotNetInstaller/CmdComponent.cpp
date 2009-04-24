c3cc9373cdf4598d92c1397481c0acf633358391
dblock <none@none.com>
2009-04-24 15:01:38 +0000

    Added log message in cmd component.

--- dotNetInstaller/CmdComponent.cpp
+++ dotNetInstaller/CmdComponent.cpp
@@ -10,10 +10,12 @@ bool cmd_component::Exec()
 {
     if (QuietInstall.IsSilent() && command_silent.GetLength() > 0)
     {
+		ApplicationLog.Write(TEXT("Executing: "), command_silent);
 	    return DVLib::ExecCmd(command_silent, &m_process_info);
     }
     else
     {
+		ApplicationLog.Write(TEXT("Executing: "), command);
 	    return DVLib::ExecCmd(command, &m_process_info);
     }
 };
