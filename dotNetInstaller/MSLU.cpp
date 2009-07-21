59c7bbee1f22fe7c18f488a9161d725148a421f0
dblock <none@none.com>
2009-07-21 04:10:03 +0000

    Embedding unicows.dll, no longer required as a separate file. Changed optimization to size, reduced footprint by half a meg.

---
+++ dotNetInstaller/MSLU.cpp
@@ -0,0 +1,60 @@
+#include "StdAfx.h"
+
+static HMODULE SaveAndLoadMSLU()
+{
+	char tf[MAX_PATH] = { 0 };
+	if (! GetTempPathA(MAX_PATH, tf)) 
+		return NULL;
+
+	if (0 != strcat_s<>(tf, "unicows.dll"))
+		return NULL;
+
+	if (GetFileAttributesA(tf) == INVALID_FILE_ATTRIBUTES)
+	{
+		HRSRC res_unicows = ::FindResourceA(NULL, "RES_UNICOWS", "CUSTOM");
+		if (res_unicows == NULL) 
+			return NULL;
+
+		HGLOBAL hgl = ::LoadResource(NULL, res_unicows);
+		if (hgl == NULL) 
+			return NULL;
+
+		DWORD size = SizeofResource(NULL, res_unicows);
+		if (size == 0) 
+			return NULL;
+
+		LPVOID buffer = LockResource(hgl);
+		
+		HANDLE h = CreateFileA(tf, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL); 
+		if (h == INVALID_HANDLE_VALUE)
+			return NULL;
+
+		DWORD written = 0;
+		if (! WriteFile(h, buffer, size, & written, NULL))
+		{
+			::CloseHandle(h);
+			return NULL;
+		}
+
+		::CloseHandle(h);
+	}
+
+	return ::LoadLibraryA(tf);
+}
+
+static HMODULE __stdcall LoadMSLU (void)
+{
+    HMODULE hUnicows = ::LoadLibraryA("unicows.dll");
+	if (hUnicows == 0) hUnicows = SaveAndLoadMSLU();
+    if (hUnicows == 0)
+    {
+		MessageBoxA(0, "Error loading unicows.dll (Microsoft Layer for Unicode, MSLU)", 
+			"dotNetInstaller", MB_ICONSTOP | MB_OK);
+		_exit(-1);
+    }
+    return hUnicows;
+}
+
+// load Microsoft Layer for Unicode (MSLU)
+// http://msdn.microsoft.com/en-us/magazine/cc301794.aspx
+extern "C" HMODULE (__stdcall *_PfnLoadUnicows) (void) = & LoadMSLU; 
