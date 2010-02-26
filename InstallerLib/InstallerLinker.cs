0d727361507053d35db92b9294554cea238d5690
dblock <none@none.com>
2010-02-26 06:28:27 +0000

    Using a single BeginUpdateResource/EndUpdateResource to work around broken/missing CAB problem.

--- InstallerLib/InstallerLinker.cs
+++ InstallerLib/InstallerLinker.cs
@@ -5,6 +5,8 @@ using System.Text;
 using Vestris.ResourceLib;
 using System.IO;
 using CabLib;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
 
 namespace InstallerLib
 {
@@ -21,25 +23,13 @@ namespace InstallerLib
             System.IO.File.Copy(args.template, args.output, true);
             System.IO.File.SetAttributes(args.output, System.IO.FileAttributes.Normal);
 
-            if (!string.IsNullOrEmpty(args.banner))
-            {
-                args.WriteLine(string.Format("Embedding banner \"{0}\"", args.banner));
-                ResourceUpdate.WriteFile(args.output, new ResourceId("CUSTOM"), new ResourceId("RES_BANNER"), 0, args.banner);
-            }
-
-            if (!string.IsNullOrEmpty(args.splash))
-            {
-                args.WriteLine(string.Format("Embedding splash screen \"{0}\"", args.splash));
-                ResourceUpdate.WriteFile(args.output, new ResourceId("CUSTOM"), new ResourceId("RES_SPLASH"), 0, args.splash);
-            }
-
-            args.WriteLine(string.Format("Embedding configuration \"{0}\"", args.config));
-            ResourceUpdate.WriteFile(args.output, new ResourceId("CUSTOM"), new ResourceId("RES_CONFIGURATION"), 0, args.config);
+            #region Version Information
 
             ConfigFile configfile = new ConfigFile();
             configfile.Load(args.config);
 
             // \todo: check XML with XSD, warn if nodes are being dropped
+
             args.WriteLine(string.Format("Updating binary attributes in \"{0}\"", args.output));
             VersionResource rc = new VersionResource();
             rc.LoadFrom(args.output);
@@ -64,6 +54,9 @@ namespace InstallerLib
             rc.Language = ResourceUtil.NEUTRALLANGID;
             rc.SaveTo(args.output);
 
+            #endregion
+
+            #region Optional Icon
             // optional icon
             if (!string.IsNullOrEmpty(args.icon))
             {
@@ -72,135 +65,193 @@ namespace InstallerLib
                 List<string> iconSizes = new List<string>();
                 foreach (IconFileIcon icon in iconFile.Icons)
                     iconSizes.Add(icon.ToString());
-                args.WriteLine(string.Format(" {0}", string.Join(", ", iconSizes.ToArray())));                
+                args.WriteLine(string.Format(" {0}", string.Join(", ", iconSizes.ToArray())));
                 IconDirectoryResource iconDirectory = new IconDirectoryResource(iconFile);
-                iconDirectory.Name = new ResourceId(128);                
+                iconDirectory.Name = new ResourceId(128);
                 iconDirectory.Language = ResourceUtil.NEUTRALLANGID;
                 iconDirectory.SaveTo(args.output);
             }
+            #endregion
 
-            string supportdir = string.IsNullOrEmpty(args.apppath)
-                ? Environment.CurrentDirectory
-                : args.apppath;
-
-            // resource files
-            ResourceFileCollection r_files = configfile.GetResources(supportdir);
-            foreach (ResourceFilePair r_pair in r_files)
-            {
-                args.WriteLine(string.Format("Embedding resource \"{0}\": {1}", r_pair.id, r_pair.path));
-                ResourceUpdate.WriteFile(args.output, new ResourceId("CUSTOM"), new ResourceId(r_pair.id), 0, r_pair.path);
-            }
-
-            // embedded files
-            if (args.embed)
-            {
-                long totalSize = 0;
-                List<String> allFilesList = new List<string>();
-
-                args.WriteLine(string.Format("Compressing files in \"{0}\"", supportdir));
-                Dictionary<string, EmbedFileCollection> all_files = configfile.GetFiles(string.Empty, supportdir);
-                // ensure at least one for additional command-line parameters
-                if (all_files.Count == 0) all_files.Add(string.Empty, new EmbedFileCollection(supportdir));
-                Dictionary<string, EmbedFileCollection>.Enumerator enumerator = all_files.GetEnumerator();
-                while(enumerator.MoveNext())
-                {
-                    EmbedFileCollection c_files = enumerator.Current.Value;
-
-                    // add additional command-line files to the root CAB
-                    if (string.IsNullOrEmpty(enumerator.Current.Key))
-                    {
-                        if (args.embedFiles != null)
-                        {
-                            foreach (string filename in args.embedFiles)
-                            {
-                                string fullpath = Path.Combine(args.apppath, filename);
-                                c_files.Add(new EmbedFilePair(fullpath, filename));
-                            }
-                        }
-
-                        if (args.embedFolders != null)
-                        {
-                            foreach (string folder in args.embedFolders)
-                            {
-                                c_files.AddDirectory(folder);
-                            }
-                        }
-                    }
-
-                    if (c_files.Count == 0)
-                        continue;
-
-                    c_files.CheckFilesExist(args);
-                    c_files.CheckFileAttributes(args);
-
-                    ArrayList files = c_files.GetFilePairs();
-
-                    // create a temporary directory for CABs
-                    string cabtemp = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-                    Directory.CreateDirectory(cabtemp);
-                    args.WriteLine(string.Format("Writing CABs to \"{0}\"", cabtemp));
-
-                    // compress new CABs
-                    string cabname = string.IsNullOrEmpty(enumerator.Current.Key)
-                        ? Path.Combine(cabtemp, "SETUP_%d.CAB")
-                        : Path.Combine(cabtemp, string.Format("SETUP_{0}_%d.CAB", enumerator.Current.Key));
-
-                    Compress cab = new Compress();
-                    long currentSize = 0;
-                    cab.evFilePlaced += delegate(string s_File, int s32_FileSize, bool bContinuation)
-                    {
-                        if (!bContinuation)
-                        {
-                            totalSize += s32_FileSize;
-                            currentSize += s32_FileSize;
-                            args.WriteLine(String.Format(" {0} - {1}", s_File, EmbedFileCollection.FormatBytes(s32_FileSize)));
-                        }
-
-                        return 0;
-                    };
-                    cab.CompressFileList(files, cabname, true, true, args.embedResourceSize);
-
-                    // embed new CABs
-                    args.WriteLine("Embedding CABs");
-                    foreach (string cabfile in Directory.GetFiles(cabtemp))
-                    {
-                        args.WriteLine(string.Format(" {0} - {1}", Path.GetFileName(cabfile),
-                            EmbedFileCollection.FormatBytes(new FileInfo(cabfile).Length)));
-                        ResourceUpdate.WriteFile(args.output, new ResourceId("RES_CAB"),
-                            new ResourceId(Path.GetFileName(cabfile)),
-                            ResourceUtil.NEUTRALLANGID, cabfile);
-                    }
-
-                    StringBuilder fileslist = new StringBuilder();
-                    fileslist.AppendLine(string.Format("{0} CAB size: {1}", 
-                        string.IsNullOrEmpty(enumerator.Current.Key) ? "*" : enumerator.Current.Key,
-                        EmbedFileCollection.FormatBytes(currentSize)));
-
-                    fileslist.Append(" " + String.Join("\r\n ", c_files.GetFileValuesWithSize(2)));
-                    allFilesList.Add(fileslist.ToString());
-
-                    args.WriteLine(string.Format("Cleaning up \"{0}\"", cabtemp));
-                    Directory.Delete(cabtemp, true);
-                }
-
-                args.WriteLine("Embedding CAB directory");
-                StringBuilder filesDirectory = new StringBuilder();
-                filesDirectory.AppendLine(string.Format("Total CAB size: {0}\r\n", EmbedFileCollection.FormatBytes(totalSize)));
-                filesDirectory.AppendLine(string.Join("\r\n\r\n", allFilesList.ToArray()));
-                byte[] filesDirectory_b = Encoding.Unicode.GetBytes(filesDirectory.ToString());
-                ResourceUpdate.Write(args.output, new ResourceId("CUSTOM"), new ResourceId("RES_CAB_LIST"),
-                    ResourceUtil.NEUTRALLANGID, filesDirectory_b);
-            }
-
-            if (! string.IsNullOrEmpty(args.manifest))
+            #region Manifest
+            if (!string.IsNullOrEmpty(args.manifest))
             {
                 args.WriteLine(string.Format("Embedding manifest \"{0}\"", args.manifest));
                 ManifestResource manifest = new ManifestResource();
                 manifest.Manifest.Load(args.manifest);
                 manifest.SaveTo(args.output);
             }
+            #endregion
 
-            args.WriteLine(string.Format("Successfully created \"{0}\"", args.output));
+            string supportdir = string.IsNullOrEmpty(args.apppath)
+                ? Environment.CurrentDirectory
+                : args.apppath;
+
+            // create a temporary directory for CABs
+            string cabtemp = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(cabtemp);
+            args.WriteLine(string.Format("Writing CABs to \"{0}\"", cabtemp));
+
+            try
+            {
+                #region Prepare CABs
+
+                long totalSize = 0;
+                List<String> allFilesList = new List<string>();
+
+                // embedded files
+                if (args.embed)
+                {
+                    args.WriteLine(string.Format("Compressing files in \"{0}\"", supportdir));
+                    Dictionary<string, EmbedFileCollection> all_files = configfile.GetFiles(string.Empty, supportdir);
+                    // ensure at least one for additional command-line parameters
+                    if (all_files.Count == 0) all_files.Add(string.Empty, new EmbedFileCollection(supportdir));
+                    Dictionary<string, EmbedFileCollection>.Enumerator enumerator = all_files.GetEnumerator();
+
+                    while (enumerator.MoveNext())
+                    {
+                        EmbedFileCollection c_files = enumerator.Current.Value;
+
+                        // add additional command-line files to the root CAB
+                        if (string.IsNullOrEmpty(enumerator.Current.Key))
+                        {
+                            if (args.embedFiles != null)
+                            {
+                                foreach (string filename in args.embedFiles)
+                                {
+                                    string fullpath = Path.Combine(args.apppath, filename);
+                                    c_files.Add(new EmbedFilePair(fullpath, filename));
+                                }
+                            }
+
+                            if (args.embedFolders != null)
+                            {
+                                foreach (string folder in args.embedFolders)
+                                {
+                                    c_files.AddDirectory(folder);
+                                }
+                            }
+                        }
+
+                        if (c_files.Count == 0)
+                            continue;
+
+                        c_files.CheckFilesExist(args);
+                        c_files.CheckFileAttributes(args);
+
+                        ArrayList files = c_files.GetFilePairs();
+
+                        // compress new CABs
+                        string cabname = string.IsNullOrEmpty(enumerator.Current.Key)
+                            ? Path.Combine(cabtemp, "SETUP_%d.CAB")
+                            : Path.Combine(cabtemp, string.Format("SETUP_{0}_%d.CAB", enumerator.Current.Key));
+
+                        Compress cab = new Compress();
+                        long currentSize = 0;
+                        cab.evFilePlaced += delegate(string s_File, int s32_FileSize, bool bContinuation)
+                        {
+                            if (!bContinuation)
+                            {
+                                totalSize += s32_FileSize;
+                                currentSize += s32_FileSize;
+                                args.WriteLine(String.Format(" {0} - {1}", s_File, EmbedFileCollection.FormatBytes(s32_FileSize)));
+                            }
+
+                            return 0;
+                        };
+                        cab.CompressFileList(files, cabname, true, true, args.embedResourceSize);
+
+                        StringBuilder fileslist = new StringBuilder();
+                        fileslist.AppendLine(string.Format("{0} CAB size: {1}",
+                            string.IsNullOrEmpty(enumerator.Current.Key) ? "*" : enumerator.Current.Key,
+                            EmbedFileCollection.FormatBytes(currentSize)));
+
+                        fileslist.Append(" " + String.Join("\r\n ", c_files.GetFileValuesWithSize(2)));
+                        allFilesList.Add(fileslist.ToString());
+                    }
+                }
+                #endregion
+
+                #region Resources
+                // embed resources
+
+                IntPtr h = ResourceUpdate.BeginUpdateResource(args.output, false);
+
+                if (h == IntPtr.Zero)
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+
+                if (!string.IsNullOrEmpty(args.banner))
+                {
+                    args.WriteLine(string.Format("Embedding banner \"{0}\"", args.banner));
+                    ResourceUpdate.WriteFile(h, new ResourceId("CUSTOM"), new ResourceId("RES_BANNER"),
+                        ResourceUtil.NEUTRALLANGID, args.banner);
+                }
+
+                if (!string.IsNullOrEmpty(args.splash))
+                {
+                    args.WriteLine(string.Format("Embedding splash screen \"{0}\"", args.splash));
+                    ResourceUpdate.WriteFile(h, new ResourceId("CUSTOM"), new ResourceId("RES_SPLASH"),
+                        ResourceUtil.NEUTRALLANGID, args.splash);
+                }
+
+                args.WriteLine(string.Format("Embedding configuration \"{0}\"", args.config));
+                ResourceUpdate.WriteFile(h, new ResourceId("CUSTOM"), new ResourceId("RES_CONFIGURATION"),
+                    ResourceUtil.NEUTRALLANGID, args.config);
+
+                // embed CABs
+                if (args.embed)
+                {
+                    args.WriteLine("Embedding CABs");
+                    foreach (string cabfile in Directory.GetFiles(cabtemp))
+                    {
+                        args.WriteLine(string.Format(" {0} - {1}", Path.GetFileName(cabfile),
+                            EmbedFileCollection.FormatBytes(new FileInfo(cabfile).Length)));
+
+                        ResourceUpdate.WriteFile(h, new ResourceId("RES_CAB"), new ResourceId(Path.GetFileName(cabfile)),
+                            ResourceUtil.NEUTRALLANGID, cabfile);
+                    }
+
+                    // cab directory
+
+                    args.WriteLine("Embedding CAB directory");
+                    StringBuilder filesDirectory = new StringBuilder();
+                    filesDirectory.AppendLine(string.Format("Total CAB size: {0}\r\n", EmbedFileCollection.FormatBytes(totalSize)));
+                    filesDirectory.AppendLine(string.Join("\r\n\r\n", allFilesList.ToArray()));
+                    byte[] filesDirectory_b = Encoding.Unicode.GetBytes(filesDirectory.ToString());
+                    ResourceUpdate.Write(h, new ResourceId("CUSTOM"), new ResourceId("RES_CAB_LIST"),
+                        ResourceUtil.NEUTRALLANGID, filesDirectory_b);
+                }
+
+                // resource files
+                ResourceFileCollection r_files = configfile.GetResources(supportdir);
+                foreach (ResourceFilePair r_pair in r_files)
+                {
+                    args.WriteLine(string.Format("Embedding resource \"{0}\": {1}", r_pair.id, r_pair.path));
+                    ResourceUpdate.WriteFile(h, new ResourceId("CUSTOM"), new ResourceId(r_pair.id),
+                        ResourceUtil.NEUTRALLANGID, r_pair.path);
+                }
+
+                args.WriteLine(string.Format("Writing {0}", EmbedFileCollection.FormatBytes(totalSize)));
+
+                if (!ResourceUpdate.EndUpdateResource(h, false))
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+
+                #endregion
+            }
+            finally
+            {
+                args.WriteLine(string.Format("Cleaning up \"{0}\"", cabtemp));
+
+                if (Directory.Exists(cabtemp))
+                {
+                    Directory.Delete(cabtemp, true);
+                }
+            }
+
+            args.WriteLine(string.Format("Successfully created \"{0}\" ({1})", 
+                args.output, EmbedFileCollection.FormatBytes(new FileInfo(args.output).Length)));
         }
     }
 }
--- InstallerLib/ResourceUpdate.cs
+++ InstallerLib/ResourceUpdate.cs
@@ -2,6 +2,7 @@ using System;
 using System.Runtime.InteropServices;
 using Vestris.ResourceLib;
 using System.IO;
+using System.ComponentModel;
 
 namespace InstallerLib
 {
@@ -10,56 +11,82 @@ namespace InstallerLib
     /// </summary>
     public abstract class ResourceUpdate
     {
+        /// <summary>
+        /// Returns a handle to either a language-neutral portable executable file (LN file) or a 
+        /// language-specific resource file (.mui file) that can be used by the UpdateResource function 
+        /// to add, delete, or replace resources in a binary module.
+        /// </summary>
+        /// <param name="pFileName">Pointer to a null-terminated string that specifies the binary file in which to update resources.</param>
+        /// <param name="bDeleteExistingResources">Specifies whether to delete the pFileName parameter's existing resources.</param>
+        /// <returns>If the function succeeds, the return value is a handle that can be used by the UpdateResource and EndUpdateResource functions.</returns>
+        [DllImport("kernel32.dll", EntryPoint = "BeginUpdateResourceW", SetLastError = true, CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+        internal static extern IntPtr BeginUpdateResource(string pFileName, bool bDeleteExistingResources);
+
+        /// <summary>
+        /// Adds, deletes, or replaces a resource in a portable executable (PE) file. 
+        /// There are some restrictions on resource updates in files that contain Resource Configuration (RC Config) data: 
+        /// language-neutral (LN) files and language-specific resource (.mui) files.
+        /// </summary>
+        /// <param name="hUpdate">A module handle returned by the BeginUpdateResource function, referencing the file to be updated.</param>
+        /// <param name="lpType">Pointer to a null-terminated string specifying the resource type to be updated.</param>
+        /// <param name="lpName">Pointer to a null-terminated string specifying the name of the resource to be updated.</param>
+        /// <param name="wLanguage">Specifies the language identifier of the resource to be updated.</param>
+        /// <param name="lpData">Pointer to the resource data to be inserted into the file indicated by hUpdate.</param>
+        /// <param name="cbData">Specifies the size, in bytes, of the resource data at lpData.</param>
+        /// <returns>Returns TRUE if successful or FALSE otherwise.</returns>
+        [DllImport("kernel32.dll", EntryPoint = "UpdateResourceW", SetLastError = true, CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+        internal static extern bool UpdateResource(IntPtr hUpdate, IntPtr lpType, IntPtr lpName, UInt16 wLanguage, byte[] lpData, UInt32 cbData);
+
+        /// <summary>
+        /// Commits or discards changes made prior to a call to UpdateResource.
+        /// </summary>
+        /// <param name="hUpdate">A module handle returned by the BeginUpdateResource function, and used by UpdateResource, referencing the file to be updated.</param>
+        /// <param name="fDiscard">Specifies whether to write the resource updates to the file. If this parameter is TRUE, no changes are made. If it is FALSE, the changes are made: the resource updates will take effect.</param>
+        /// <returns>Returns TRUE if the function succeeds; FALSE otherwise.</returns>
+        [DllImport("kernel32.dll", EntryPoint = "EndUpdateResourceW", SetLastError = true, CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+        internal static extern bool EndUpdateResource(IntPtr hUpdate, bool fDiscard);
+
         internal static void WriteFile(
-            string output,
+            IntPtr h,
             ResourceId resourceType,
             ResourceId resourceName,
             UInt16 resourceLanguage, 
             string input)
-        {
-            GenericResource resource = new GenericResource(resourceType, resourceName, resourceLanguage);
-            
-            try
+        {            
+            using (FileStream binaryStream = new FileStream(input, FileMode.Open, FileAccess.Read))
             {
-                using (FileStream binaryStream = new FileStream(input, FileMode.Open, FileAccess.Read))
-                {
-                    resource.Data = new byte[binaryStream.Length];
-                    binaryStream.Read(resource.Data, 0, (int)binaryStream.Length);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(string.Format("Error reading {0}: {1}", input, ex.Message),
-                    ex);
-            }
+                byte[] data = null;
 
-            try
-            {
-                resource.SaveTo(output);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(string.Format("Error writing {0}: {1}", output, ex.Message),
-                    ex);
+                try
+                {
+                    data = new byte[binaryStream.Length];
+                    binaryStream.Read(data, 0, (int)binaryStream.Length);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(string.Format("Error reading {0}: {1}", input, ex.Message),
+                        ex);
+                }
+
+                Write(h, resourceType, resourceName, resourceLanguage, data);
             }
         }
 
         internal static void Write(
-            string output,
+            IntPtr h,
             ResourceId resourceType,
             ResourceId resourceName,
             UInt16 resourceLanguage,
             byte[] buffer)
         {
-            GenericResource resource = new GenericResource(resourceType, resourceName, resourceLanguage);
-            resource.Data = buffer;
             try
             {
-                resource.SaveTo(output);
+                if (!UpdateResource(h, resourceType.Id, resourceName.Id, resourceLanguage, buffer, (uint) buffer.Length))
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
             }
             catch (Exception ex)
             {
-                throw new Exception(string.Format("Error writing {0}: {1}", output, ex.Message),
+                throw new Exception(string.Format("Error writing {0}: {1}", resourceName, ex.Message),
                     ex);
             }
         }
