c0a5c51f2fe495d79dd61d3d56077f76a0e0762b
dblock <none@none.com>
2010-08-08 17:50:44 +0000

    Using an MFC app as initializer for htmlinstaller, fixes error dialogs. Added documentation on customizing HTML.

--- htmlInstaller/htmlInstaller.cpp
+++ htmlInstaller/htmlInstaller.cpp
@@ -6,124 +6,98 @@
 #include "ExtractCabProcessor.h"
 #include <Version/Version.h>
 
-#define MAX_LOADSTRING 100
+BEGIN_MESSAGE_MAP(CHtmlInstallerApp, CWinApp)
+	ON_COMMAND(ID_HELP, CWinApp::OnHelp)
+END_MESSAGE_MAP()
 
-HINSTANCE hInst; // current instance
-TCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
-
-BOOL InitInstance(HINSTANCE, int);
-void ExitInstance();
-void ParseCommandLine(CCommandLineInfo& rCmdInfo);
-void DisplayHelp();
-void DisplayCab();
-void ExtractAllCabs();
-void DisplayConfig();
-void ExtractCab(const std::wstring& id);
-
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+CHtmlInstallerApp::CHtmlInstallerApp()
+	: m_rc(0)
 {
+
+}
+
+CHtmlInstallerApp theApp;
+
+BOOL CHtmlInstallerApp::InitInstance()
+{
+	InitCommonControls();
+	CWinApp::InitInstance();
+
 	try
 	{
-		UNREFERENCED_PARAMETER(hPrevInstance);
-		UNREFERENCED_PARAMETER(lpCmdLine);
+		reset(InstallerCommandLineInfo::Instance, new InstallerCommandLineInfo());
+		reset(InstallerLauncher::Instance, new InstallerLauncher());
+		reset(InstallerLog::Instance, new InstallerLog());
+		reset(InstallerSession::Instance, new InstallerSession());
+		reset(InstallUILevelSetting::Instance, new InstallUILevelSetting());
+		reset(HtmLayoutDll::Instance, new HtmLayoutDll());
 
-		// Initialize global strings
-		LoadString(hInstance, IDC_HTMLINSTALLER, szWindowClass, MAX_LOADSTRING);
+		HtmlWindow::RegisterClass(m_hInstance);
 
-		// Perform application initialization:
-		if (! InitInstance(hInstance, nCmdShow))
+		ParseCommandLine(* get(InstallerCommandLineInfo::Instance));
+			
+		CSplashWnd::EnableSplashScreen(InstallerCommandLineInfo::Instance->DisplaySplash());
+
+		LOG(L"-------------------------------------------------------------------");
+		LOG(L"htmlNetInstaller (DNI) started, version " << TEXT(VERSION_VALUE));
+		LOG(VERSION_LEGALCOPYRIGHT_VALUE);
+		LOG(L"Operating system: " << DVLib::GetOperatingSystemVersionString());
+		LOG(L"-------------------------------------------------------------------");
+		LOG(L"Sequence: " << InstallSequenceUtil::towstring(InstallerSession::Instance->sequence));
+
+		std::map<std::wstring, std::wstring>::iterator arg = InstallerCommandLineInfo::Instance->componentCmdArgs.begin();
+		while(arg != InstallerCommandLineInfo::Instance->componentCmdArgs.end())
 		{
-			ExitInstance();
-			return 0;
+			LOG(L"Component arguments: \"" + arg->first + L"\": " << arg->second);
+			arg ++;
+		}
+
+		// propagate command line arguments during execution
+		InstallerSession::Instance->AdditionalCmdLineArgs = InstallerCommandLineInfo::Instance->componentCmdArgs;
+		InstallerSession::Instance->AdditionalControlArgs = InstallerCommandLineInfo::Instance->controlCmdArgs;
+
+		if (InstallerCommandLineInfo::Instance->DisplayHelp())
+		{
+			DisplayHelp();
+			return FALSE;
+		}
+
+		// just display CAB contents
+		if (InstallerCommandLineInfo::Instance->DisplayCab())
+		{
+			DisplayCab();
+			return FALSE;
+		}
+		// just extract the CABs
+		else if (InstallerCommandLineInfo::Instance->ExtractCab())
+		{
+			ExtractAllCabs();
+			return FALSE;
+		}
+		// just display the configuration
+		else if (InstallerCommandLineInfo::Instance->DisplayConfig())
+		{
+			DisplayConfig();
+			return FALSE;
 		}
 
 		ConfigFileManagerPtr config(new ConfigFileManager());
 		config->Load();
 		config->Run();
-		int rc = config->GetRecordedError();
+		m_rc = config->GetRecordedError();
 		reset(config);
-		TRYLOG(L"htmlInstaller finished, return code=" << rc);
-		ExitInstance();
-		return rc;
 	}
 	catch(std::exception& ex)
 	{
         DniMessageBox::Show(DVLib::string2wstring(ex.what()).c_str(), MB_OK|MB_ICONSTOP);
 		TRYLOG(L"Error: " << DVLib::string2wstring(ex.what()));
-		ExitInstance();
-		return -1;
+		m_rc = -1;
 	}
+
+	return FALSE;
 }
 
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-	hInst = hInstance; // Store instance handle in our global variable
-
-	reset(InstallerCommandLineInfo::Instance, new InstallerCommandLineInfo());
-	reset(InstallerLauncher::Instance, new InstallerLauncher());
-	reset(InstallerLog::Instance, new InstallerLog());
-	reset(InstallerSession::Instance, new InstallerSession());
-	reset(InstallUILevelSetting::Instance, new InstallUILevelSetting());
-	reset(HtmLayoutDll::Instance, new HtmLayoutDll());
-
-	HtmlWindow::RegisterClass(hInstance);
-
-	ParseCommandLine(* get(InstallerCommandLineInfo::Instance));
-		
-	CSplashWnd::EnableSplashScreen(InstallerCommandLineInfo::Instance->DisplaySplash());
-
-	LOG(L"-------------------------------------------------------------------");
-	LOG(L"htmlNetInstaller (DNI) started, version " << TEXT(VERSION_VALUE));
-	LOG(VERSION_LEGALCOPYRIGHT_VALUE);
-	LOG(L"Operating system: " << DVLib::GetOperatingSystemVersionString());
-	LOG(L"-------------------------------------------------------------------");
-	LOG(L"Sequence: " << InstallSequenceUtil::towstring(InstallerSession::Instance->sequence));
-
-	std::map<std::wstring, std::wstring>::iterator arg = InstallerCommandLineInfo::Instance->componentCmdArgs.begin();
-	while(arg != InstallerCommandLineInfo::Instance->componentCmdArgs.end())
-	{
-		LOG(L"Component arguments: \"" + arg->first + L"\": " << arg->second);
-		arg ++;
-	}
-
-	// propagate command line arguments during execution
-	InstallerSession::Instance->AdditionalCmdLineArgs = InstallerCommandLineInfo::Instance->componentCmdArgs;
-	InstallerSession::Instance->AdditionalControlArgs = InstallerCommandLineInfo::Instance->controlCmdArgs;
-
-	if (InstallerCommandLineInfo::Instance->DisplayHelp())
-	{
-		DisplayHelp();
-		return FALSE;
-	}
-
-	// just display CAB contents
-	if (InstallerCommandLineInfo::Instance->DisplayCab())
-	{
-		DisplayCab();
-		return FALSE;
-	}
-	// just extract the CABs
-	else if (InstallerCommandLineInfo::Instance->ExtractCab())
-	{
-		ExtractAllCabs();
-		return FALSE;
-	}
-	// just display the configuration
-	else if (InstallerCommandLineInfo::Instance->DisplayConfig())
-	{
-		DisplayConfig();
-		return FALSE;
-	}
-
-	return TRUE;
-}
-
-void ExitInstance()
+int CHtmlInstallerApp::ExitInstance() 
 {
 	reset(HtmLayoutDll::Instance);
 	reset(InstallerCommandLineInfo::Instance);
@@ -131,37 +105,23 @@ void ExitInstance()
 	reset(InstallerLog::Instance);
 	reset(InstallerSession::Instance);
 	reset(InstallUILevelSetting::Instance);
+
+	CWinApp::ExitInstance();
+	return m_rc;
 }
 
-void ParseCommandLine(CCommandLineInfo& rCmdInfo)
-{
-	for (int i = 1; i < __argc; i++)
-	{
-		LPCTSTR pszParam = __targv[i];
-		BOOL bFlag = FALSE;
-		BOOL bLast = ((i + 1) == __argc);
-		if (pszParam[0] == '-' || pszParam[0] == '/')
-		{
-			// remove flag specifier
-			bFlag = TRUE;
-			++pszParam;
-		}
-		rCmdInfo.ParseParam(pszParam, bFlag, bLast);
-	}
-}
-
-void DisplayHelp()
+void CHtmlInstallerApp::DisplayHelp()
 {
 	DniMessageBox::Show(InstallerCommandLineInfo::Instance->GetUsage(), MB_OK|MB_ICONINFORMATION);
 }
 
-void DisplayCab()
+void CHtmlInstallerApp::DisplayCab()
 {
 	ExtractCabProcessorPtr extractcab(new ExtractCabProcessor(InstallerWindow::s_hinstance, L"", NULL));
 	DniMessageBox::Show(DVLib::join(extractcab->GetCabFiles(), L"\r\n"), MB_OK|MB_ICONINFORMATION);
 }
 
-void ExtractAllCabs()
+void CHtmlInstallerApp::ExtractAllCabs()
 {
 	ExtractCab(L"");
 	ConfigFileManagerPtr config(new ConfigFileManager());
@@ -179,7 +139,7 @@ void ExtractAllCabs()
 	}
 }
 
-void ExtractCab(const std::wstring& id)
+void CHtmlInstallerApp::ExtractCab(const std::wstring& id)
 {
 	ExtractCabProcessorPtr p_extractcab(new ExtractCabProcessor(InstallerWindow::s_hinstance, id, NULL));	
 	int cab_count = p_extractcab->GetCabCount();
@@ -197,10 +157,20 @@ void ExtractCab(const std::wstring& id)
 	p_extractcab->EndExec();
 }
 
-void DisplayConfig()
+void CHtmlInstallerApp::DisplayConfig()
 {
 	ConfigFileManagerPtr config(new ConfigFileManager());
 	config->Load();
 	DniMessageBox::Show(config->GetString(), MB_OK|MB_ICONINFORMATION);
 }
 
+BOOL CHtmlInstallerApp::PreTranslateMessage(MSG * pMsg)
+{
+	// route messages to the splash screen while it is visible
+	if (CSplashWnd::PreTranslateAppMessage(pMsg)) 
+	{
+		return TRUE;
+	}
+
+	return CWinApp::PreTranslateMessage(pMsg);
+}
--- htmlInstaller/htmlInstaller.h
+++ htmlInstaller/htmlInstaller.h
@@ -1,3 +1,22 @@
 #pragma once
 
 #include "resource.h"
+
+class CHtmlInstallerApp : public CWinApp
+{
+private:
+	int m_rc;
+	void DisplayHelp();
+    void ExtractAllCabs();
+	void ExtractCab(const std::wstring& id);
+	void DisplayCab();
+	void DisplayConfig();
+public:
+	CHtmlInstallerApp();
+	BOOL InitInstance();
+	int ExitInstance();
+	BOOL PreTranslateMessage(MSG *);
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CHtmlInstallerApp theApp;
