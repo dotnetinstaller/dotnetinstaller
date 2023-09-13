b3357020ffc937dd65a309bb4189e54888c7ca81
Rami <icnocop@users.noreply.github.com>
2023-09-12 23:16:03 -0700

    Enable remote connection for debugging failed test

---
+++ build/TeamViewer/EnableRemoteConnection.ps1
@@ -0,0 +1,35 @@
+﻿# Enable -Verbose option
+[CmdletBinding()]
+
+# Set a flag to force verbose as a default
+$VerbosePreference ='Continue' # equiv to -verbose
+
+.\build\ProcessRunner.ps1
+
+$exitCode = Run-Process -FilePath "reg.exe" -ArgumentList "import "".\build\TeamViewer\TeamViewer_Settings.reg"""
+if ($exitCode -ne 0)
+{  
+    throw "Command failed with exit code $exitCode."  
+}  
+
+# Install TeamViewer
+$exitCode = Run-Process -FilePath "choco.exe" -ArgumentList "install teamviewer -y"
+if ($exitCode -ne 0)
+{  
+    throw "Command failed with exit code $exitCode."  
+}  
+
+Start-Process -FilePath "${env:ProgramFiles(x86)}\TeamViewer\TeamViewer.exe"
+
+# get User ID
+$key = 'HKLM:\SOFTWARE\WOW6432Node\TeamViewer'
+
+while (!(Get-ItemProperty -Path $key -Name "ClientID" -ErrorAction SilentlyContinue))
+{
+    start-sleep -seconds 5
+}
+
+$clientId = (Get-ItemProperty -Path $key -Name "ClientID").ClientID
+
+Write-Host "TeamViewer connection details:" -ForegroundColor Yellow
+Write-Host "  Partner ID: $clientId" -ForegroundColor Gray
